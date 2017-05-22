a5db7a36412ff041463682d1565245bc9dbe1ea3
vikasbelote <vikasb0205@gmail.com>
2017-05-23 01:05:02 +0530

    245

--- src/main/resources/db_file.sql
+++ src/main/resources/db_file.sql
@@ -1,5 +1,8 @@
-
-create table my_user
+create table my_user_vikas
 (
 	user_id int primary key, user_name varchar(100)
 );
+
+insert into my_user_vikas values(1, 'vikas');
+insert into my_user_vikas values(2, 'sagar');
+insert into my_user_vikas values(3, 'sujata');
