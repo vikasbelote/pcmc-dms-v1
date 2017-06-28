45259525cbcd6ee1ed3d920d9105adc93768d470
vikasbelote <vikasb0205@gmail.com>
2017-06-28 23:46:39 +0530

    creating table for vehicle wrokshop

--- src/main/resources/db_file.sql
+++ src/main/resources/db_file.sql
@@ -24,3 +24,17 @@ create table dms_prabhag_name
 
 INSERT INTO `dms`.`dms_dept` (`dept_id`, `dept_name`, `tbl_name`, `route_url`) VALUES ('8', 'Vivah Nodhani', 'dms_vivah_nodhani', 'vivahNodhani');
 INSERT INTO `dms`.`dms_dept_login` (`login_id`, `user_name`, `user_pwd`, `dept_id`, `print`, `download`, `update_file`, `mandatory_pwd`) VALUES ('7', 'vivah', 'vivah', '8', '0', '0', '0', '0');
+
+create table dms_vehicle_workshop
+(
+	 row_id int auto_increment
+    ,table_no varchar(10)
+    ,nasti_name varchar(100)
+    ,nasti_no varchar(100)
+    ,image_path varchar(100)
+    
+    ,primary key(row_id)
+);
+
+INSERT INTO `dms`.`dms_dept` (`dept_id`, `dept_name`, `tbl_name`, `route_url`) VALUES ('9', 'Vehicle Workshop', 'dms_vehicle_workshop', 'vehicleWorkshop');
+INSERT INTO `dms`.`dms_dept_login` (`login_id`, `user_name`, `user_pwd`, `dept_id`, `print`, `download`, `update_file`, `mandatory_pwd`) VALUES ('8', 'vehworkshop', 'veh', '9', '0', '0', '0', '0');
