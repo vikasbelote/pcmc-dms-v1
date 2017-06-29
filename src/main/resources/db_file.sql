2973a3539b5cc533cb707920d696b69785d25431
vikasbelote <vikasb0205@gmail.com>
2017-06-29 23:07:27 +0530

    Creating krida vibhag table

--- src/main/resources/db_file.sql
+++ src/main/resources/db_file.sql
@@ -38,3 +38,15 @@ create table dms_vehicle_workshop
 
 INSERT INTO `dms`.`dms_dept` (`dept_id`, `dept_name`, `tbl_name`, `route_url`) VALUES ('9', 'Vehicle Workshop', 'dms_vehicle_workshop', 'vehicleWorkshop');
 INSERT INTO `dms`.`dms_dept_login` (`login_id`, `user_name`, `user_pwd`, `dept_id`, `print`, `download`, `update_file`, `mandatory_pwd`) VALUES ('8', 'vehworkshop', 'veh', '9', '0', '0', '0', '0');
+
+create table dms_krida
+(
+	 row_id int auto_increment
+    ,nasti_number varchar(100)
+    
+    ,primary key(row_id)
+);
+
+INSERT INTO `dms`.`dms_dept` (`dept_id`, `dept_name`, `tbl_name`, `route_url`) VALUES ('10', 'Krida Vibhag', 'dms_krida', 'kridaVibhag');
+INSERT INTO `dms`.`dms_dept_login` (`login_id`, `user_name`, `user_pwd`, `dept_id`, `print`, `download`, `update_file`, `mandatory_pwd`) VALUES ('9', 'krida', 'krida', '10', '0', '0', '0', '0');
+
